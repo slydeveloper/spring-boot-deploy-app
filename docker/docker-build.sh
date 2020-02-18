d0944ca080048b7b3d004b6bb9a4f5fa793de730
Sylwester Sokołowski <sylwek.sokolowski@gmail.com>
2020-02-18 21:27:06 +0100

    Add fir for Docker build script

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 source .env
-
-docker-compose down
-
-echo Building image: $DOCKER_IMAGE_NAME
-
-docker-compose build
-
-docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
-
-docker tag $DOCKER_IMAGE_NAME:latest $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
-
-docker images $DOCKER_IMAGE_NAME
-
-
-
-
-#!/bin/bash
-
-source .env
 echo Building image: $DOCKER_IMAGE_NAME
 
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
